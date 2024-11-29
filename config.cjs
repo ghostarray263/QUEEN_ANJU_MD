2d44f9f490182c040914f9de6ffffd8b27bcc7f1
ghostarray263 <alphawolf45263@gmail.com>
2024-11-29 16:50:06 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   "PREFIX": ".",
   "AUTO_READ_STATUS": true,
   "MODE": "public",
-  "BOT_NUMBER": "94717775628",
+  "BOT_NUMBER": "263713328548",
   "OMDB_API_KEY": "76cb7f39",
   "READ_CMD": true,
   "AUTO_VOICE": false,
